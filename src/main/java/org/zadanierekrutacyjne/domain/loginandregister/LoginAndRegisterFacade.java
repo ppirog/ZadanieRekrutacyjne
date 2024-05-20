5ecc2958aac9126d67238b86c4852d101dbc999d
ppirog <pawelpirog99@gmail.com>
2024-05-20 19:33:49 +0200

    1) created project structure 2) set up PostgreSql 3) set up Liquibase

---
+++ src/main/java/org/zadanierekrutacyjne/domain/loginandregister/LoginAndRegisterFacade.java
@@ -0,0 +1,9 @@
+package org.zadanierekrutacyjne.domain.loginandregister;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@AllArgsConstructor
+public class LoginAndRegisterFacade {
+}
---
+++ src/test/java/org/zadanierekrutacyjne/domain/loginandregister/LoginAndRegisterFacadeTest.java
@@ -0,0 +1,7 @@
+package org.zadanierekrutacyjne.domain.loginandregister;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginAndRegisterFacadeTest {
+
+}
