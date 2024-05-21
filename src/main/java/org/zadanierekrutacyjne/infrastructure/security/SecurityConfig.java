da92b1b2c42ccb1d7b5a7bde6ff525c656e1c744
ppirog <pawelpirog99@gmail.com>
2024-05-21 14:49:44 +0200

    deleted unused imports

--- src/main/java/org/zadanierekrutacyjne/infrastructure/security/SecurityConfig.java
+++ src/main/java/org/zadanierekrutacyjne/infrastructure/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package org.zadanierekrutacyjne.infrastructure.security;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
