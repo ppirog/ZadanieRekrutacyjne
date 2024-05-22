79cea62ded900a7ef0134bd806e6df4e3727cd01
ppirog <pawelpirog99@gmail.com>
2024-05-22 11:29:13 +0200

    adding loggers S01E01

--- src/main/java/org/zadanierekrutacyjne/infrastructure/loginandregister/controller/LoginAndRegisterRestController.java
+++ src/main/java/org/zadanierekrutacyjne/infrastructure/loginandregister/controller/LoginAndRegisterRestController.java
@@ -2,6 +2,7 @@ package org.zadanierekrutacyjne.infrastructure.loginandregister.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,7 +20,7 @@ import org.zadanierekrutacyjne.infrastructure.loginandregister.controller.dto.Re
 
 
 @RestController
-@Log
+@Log4j2
 @AllArgsConstructor
 public class LoginAndRegisterRestController {
 
@@ -31,21 +32,27 @@ public class LoginAndRegisterRestController {
     public ResponseEntity<RegisterResponseDto> registerUser(@RequestBody RegisterRequestDto registerRequestDto) {
         final UserResponseDto userResponseDto = loginAndRegisterFacade.register(mapper.fromReqisterRequestDto(registerRequestDto));
         final RegisterResponseDto registered = mapper.fromUserResponseDto(userResponseDto, "REGISTERED");
+        log.info("User registered: " + registered);
         return ResponseEntity.status(HttpStatus.CREATED).body(registered);
     }
 
     @GetMapping("/find/{login}")
     public ResponseEntity<UserResponseDto> findUser(@PathVariable String login) {
-        return ResponseEntity.ok(loginAndRegisterFacade.findByUsername(login));
+        final UserResponseDto byUsername = loginAndRegisterFacade.findByUsername(login);
+        log.info("User found: " + byUsername);
+        return ResponseEntity.ok(byUsername);
     }
 
     @PutMapping("/update/{login}")
     public ResponseEntity<UserResponseDto> updateUser(@PathVariable String login, @RequestBody RegisterRequestDto registerRequestDto) {
-        return ResponseEntity.ok(loginAndRegisterFacade.updateByLogin(login, mapper.fromReqisterRequestDto(registerRequestDto)));
+        final UserResponseDto body = loginAndRegisterFacade.updateByLogin(login, mapper.fromReqisterRequestDto(registerRequestDto));
+        log.info("User updated: " + body);
+        return ResponseEntity.ok(body);
     }
 
     @DeleteMapping("/delete/{login}")
     public ResponseEntity<UserResponseDto> deleteUser(@PathVariable String login) {
+        log.info("Deleting user: " + login);
         return ResponseEntity.ok(loginAndRegisterFacade.deleteUser(login));
     }
 
